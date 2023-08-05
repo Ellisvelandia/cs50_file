3055eef366f863bb8178a9d956976f54f4a4bcc9
Ellisvelandia <eyis619l@gmail.com>
2023-08-05 00:09:46 -0500

    sortedd

--- names.py
+++ names.py
@@ -1,12 +1,35 @@
-# names = []
+# # # # # # names = []
 
-# for _ in range(3):
-#   names.append(input("whats your name? "))
+# # # # # # for _ in range(3):
+# # # # # #   names.append(input("whats your name? "))
 
-#   for name in sorted(names):
-#     print(f"hello, {name}")
-name = input("whats youe name? ")
+# # # # # #   for name in sorted(names):
+# # # # # #     print(f"hello, {name}")
+# # # # # name = input("whats youe name? ")
 
-file = open("names.txt", "a")
-file.write(name)
-file.close()
+# # # # # with open("names.txt", "a") as file:
+# # # # #     file.write(f"{name}\n")
+# # # # # # file.close()
+
+# # # # with open("names.txt", "r") as file:
+# # # #     lines = file.readlines()
+
+# # # # for line in lines:
+# # # #     print("hello, ", line.rstrip())
+
+# # # with open("names.txt", "r") as file:
+# # #     for line in file:
+# # #         print("hello," , line.rstrip())
+
+# # names = []
+
+# # with open("names.txt") as file:
+# #     for line in file:
+# #         names.append(line.rstrip())
+
+# # for name in sorted(names):
+# #     print(f"hello , {name}")        
+
+# with open("names.txt") as file:
+#     for line in sorted(file):
+#         print("hello")
