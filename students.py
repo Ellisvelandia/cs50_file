97dd2b7fa4711545d1b009f3a3546e0e7dc8f07f
Ellisvelandia <eyis619l@gmail.com>
2023-08-07 19:08:51 -0500

    dicwritter

--- students.py
+++ students.py
@@ -1,24 +1,38 @@
+# import csv
+
+# # with open("students.csv") as file:
+# #   for line in file:
+# #    name, house = line.rstrip().split(",")
+# #    print(f"{name} is in {house}")
+
+# students = []
+
 # with open("students.csv") as file:
-#   for line in file:
-#    name, house = line.rstrip().split(",")
-#    print(f"{name} is in {house}")
+#     # for line in file:
+#     #     name, home = line.rstrip().split(",")
+#     #     student = {"name": name, "home": home}
+#     #     # student["name"] = name
+#     #     # student["house"] = house
+#     #     students.append(student)
+#     reader = csv.DictReader(file)
+#     for row  in reader:
+#         students.append({"name": row["name"], "home": row["home"]})
 
-students = []
-
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        # student["name"] = name
-        # student["house"] = house
-        students.append(student)
-
-# def get_name(student):
-#     return student["house"]
+# # def get_name(student):
+# #     return student["house"]
     
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")        
-#     students.append(f"{name} is in {house}")
+# for student in sorted(students, key=lambda student: student["name"]):
+#     print(f"{student['name']} from {student['home']}")        
+# #     students.append(f"{name} is in {house}")
 
-# for student in sorted(students):
-#   print(student)
+# # for student in sorted(students):
+# #   print(student)
+
+import csv
+
+name = input("whats your name?")
+home = input("where your name?")
+
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({ "home": home,"name": name})
